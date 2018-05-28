1d85677cdfaee2c8526654d03e95516055e8f5ea
Yuragranovsky <yuragranovsky@gmail.com>
2018-05-28 19:54:29 +0300

    Tests before break 28/05/2018

--- 18_05_24_RentCompany/src/telran/cars/tests/RentCompanyTests.java
+++ 18_05_24_RentCompany/src/telran/cars/tests/RentCompanyTests.java
@@ -2,6 +2,8 @@ package telran.cars.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import telran.cars.model.*;
@@ -14,10 +16,11 @@ private static final String MODEL2 = "B4";
 private static final String REG_NUMBER3 = "124";
 private static final long LICENSE1 = 123;
 private static final long LICENSE2 = 124;
+private static final LocalDate RENT_DATE1 = LocalDate.parse("2018-05-28");
+private static final int RENT_DAYS1 = 5;
 IRentCompany company;
 Car car1=new Car(REG_NUMBER1, "red", MODEL1);
 Car car2=new Car(REG_NUMBER2, "green", MODEL2);
-;
 Car car3=new Car(REG_NUMBER3,"silver",MODEL1);
 Model model1=new Model(MODEL1, 55, "BMW", "Germany", 200);
 Model model2=new Model(MODEL2, 50, "Subaru", "Japan", 190);
@@ -31,8 +34,33 @@ Driver driver2=new Driver(LICENSE2,"David",1960,"050-7654321");
 		company.addModel(model1);
 		company.addDriver(driver1);
 		company.addCar(car1);
+		company.rentCar(REG_NUMBER1, LICENSE1, RENT_DATE1,
+				RENT_DAYS1);
 	}
-
+	@Test
+	public void rentCar() {
+		assertEquals(CarsReturnCode.CAR_IN_USE,
+				company.rentCar(REG_NUMBER1, LICENSE1, RENT_DATE1, RENT_DAYS1));
+		assertEquals(CarsReturnCode.NO_CAR,
+				company.rentCar(REG_NUMBER2, LICENSE1, RENT_DATE1, RENT_DAYS1));
+		company.addModel(model2);
+		company.addCar(car2);
+		assertEquals(CarsReturnCode.NO_DRIVER,company.rentCar
+				(REG_NUMBER2, LICENSE2, RENT_DATE1, RENT_DAYS1));
+		assertEquals(CarsReturnCode.OK,company.rentCar
+				(REG_NUMBER2, LICENSE1, RENT_DATE1, RENT_DAYS1));
+		RentRecord record1=getRecord(REG_NUMBER1);
+		assertEquals(LICENSE1,record1.getLicenseId());
+		assertEquals(REG_NUMBER1,record1.getCarNumber());
+		assertEquals(RENT_DATE1,record1.getRentDate());
+		assertEquals(null,record1.getReturnDate());
+		assertEquals(RENT_DAYS1,record1.getRentDays());
+		
+	}
+	private RentRecord getRecord(String regNumber1) {
+		return company.getAllRecords().filter(x->x.getCarNumber()
+				.equals(regNumber1)).findFirst().get();
+	}
 	@Test
 	public void addCar() {
 		assertEquals(CarsReturnCode.CAR_EXISTS,company.addCar(car1));
